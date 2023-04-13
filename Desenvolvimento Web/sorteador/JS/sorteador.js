e26e69fe0184bab778fb17e7994e4d36d1a9f2ba
Isadora <103513099+isadorahaime@users.noreply.github.com>
2023-04-13 13:36:16 -0300

    Delete Desenvolvimento Web/sorteador directory

--- Desenvolvimento Web/sorteador/JS/sorteador.js
+++
@@ -1,7 +0,0 @@
-function sortearNum(){
-    let num1 = parseInt(document.getElementById("num1").value)
-    let num2 = parseInt(document.getElementById("num2").value)
-
-    let numSorteado = Math.floor(Math.random() * (num1 - num2 + 1)) + num2;
-    document.getElementById("resultado").innerHTML = "O número sorteado é " + numSorteado;
-}
